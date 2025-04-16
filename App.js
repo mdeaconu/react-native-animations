0c1d1428e3123deb7567022a826b31124b477b20
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-04-16 15:29:40 +0300

    feat(animation): interpolation and clamping
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { React } from "react";
-import { View } from "react-native";
-import { CIRCLE_RADIUS, s } from "./App.style";
+import { Dimensions, View } from "react-native";
+import { CIRCLE_RADIUS, s, SQUARE_SIZE } from "./App.style";
 import Animated, {
+  Extrapolation,
+  interpolate,
   useAnimatedStyle,
   useSharedValue,
   withSpring
@@ -11,6 +13,8 @@ import {
   GestureDetector,
   GestureHandlerRootView
 } from "react-native-gesture-handler";
+
+const { width: SCREEN_W } = Dimensions.get("screen");
 
 const App = () => {
   const squareAnimTranslateX = useSharedValue(0);
@@ -32,13 +36,29 @@ const App = () => {
     });
 
   const squareAnimStyle = useAnimatedStyle(() => {
+    const scale = interpolate(
+      squareAnimTranslateX.value,
+      [0, (SCREEN_W / 2 - SQUARE_SIZE / 2)],
+      [1, 3],
+      Extrapolation.CLAMP // { extrapolateRight: "clamp", extrapolateLeft: "clamp" }
+    );
+    const opacity = interpolate(
+      squareAnimTranslateX.value,
+      [0, (SCREEN_W / 2 - SQUARE_SIZE / 2)],
+      [1, 0],
+      Extrapolation.CLAMP
+    );
+
     return {
       transform: [
         { translateX: squareAnimTranslateX.value },
-        { translateY: squareAnimTranslateY.value }
+        { translateY: squareAnimTranslateY.value },
+        { scale },
       ],
+      opacity
     };
   });
+
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
